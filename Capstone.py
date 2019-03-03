6ef01b369326fae6e4a7de7e2a11f5bd6ee7e86d
Manish Thapa <manish.bu.met@gmail.com>
2019-03-02 18:53:51 -0600

    add capstone file

---
+++ Capstone.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# <h1>Capstone Project</h1>
+# 
+# <p> This notebook will be mainly used for the capstone project.</p>
+
+# In[1]:
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
